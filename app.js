7e3d8bf8dc8db3a6ed8be407af50d71acb67a6ef
Yashoda Ghimire <yashodaghimire@stucop1yghimire.attlocal.net>
2021-07-17 19:26:03 -0500

    question 5

--- app.js
+++ app.js
@@ -85,17 +85,15 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 let testArray = [a, b, c]; //eslint-disable-line
 let sumArr = testArray[0] + testArray[1] + testArray[2];
-let string =  testArray[0],testArray[1],testArray[2] + ' was passed in as an array of numbers ' + ' and ' + sumArr + ' is ' + ' their sum ' + '.';
-
 function sumArray(testArray) { //eslint-disable-line
 console.log(sumArr);
-return [sumArr, string];
+return [sumArr, (testArray[0],testArray[1],testArray[2] + ' was passed in as an array of numbers ' + ' and ' + sumArr + ' is ' + ' their sum ' + '.')];
 
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -110,12 +108,15 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
+let testArray = [a, b, c];
+let multArr =  testArray[0] * testArray[1] * testArray[2];
 function multiplyArray(multArr) { //eslint-disable-line
-
+console.log(multArr);
+return[multArr, 'The numbers ' + a, +b, +c + ' have a product of ' + multArr + '.'];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
